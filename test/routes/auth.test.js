9c297b84732db9fc93c45acc09d334d87b003723
Zach Butler <zach.standley93@gmail.com>
2019-07-29 12:04:44 -0700

    update auth test and route for signin

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -1,26 +1,9 @@
-require('dotenv').config();
-
-const request = require('supertest');
-const app = require('../lib/app');
-const connect = require('../lib/utils/connect');
-const mongoose = require('mongoose');
-let agent = request.agent(app);
+const { getAgent, getUser } = require('../data-helpers');
 
 describe('users routes', () => {
-  beforeAll(() => {
-    connect();
-  });
-
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
-  });
-
-  afterAll(() => {
-    return mongoose.connection.close();
-  });
-
+  
   it('creates and returns new user', () => {
-    return agent
+    return getAgent()
       .post('/api/v1/auth/signup')
       .send({
         username: 'bandaholic',
@@ -30,16 +13,30 @@ describe('users routes', () => {
         phone: '555-555-5555'
       })
       .then(res => {
-        console.log(res.header);
-        if(res.header) {
-          expect(res.header['set-cookie']).toBeDefined();
-        }
+        expect(res.header['set-cookie']).toBeDefined();
         expect(res.body).toEqual({
           _id: expect.any(String),
           username: 'bandaholic',
           photoUrl: 'http://photo.jpg',
           email: 'bandaholic@gmail.com',
           phone: '555-555-5555'
+        });
+      });
+  });
+
+  it('signs in a user, returns user', () => {
+    const user = getUser();
+    return getAgent()
+      .post('/api/v1/auth/signin')
+      .send({
+        username: user.username,
+        password: 'password'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: user._id.toString(),
+          username: user.username,
+          photoUrl: user.photoUrl
         });
       });
   });
