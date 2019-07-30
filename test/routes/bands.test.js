cfe83c1f3b2416a8331a679ed0f88728690a6e3f
Zach Butler <zach.standley93@gmail.com>
2019-07-30 16:58:01 -0700

    removed leaders from create

--- test/routes/bands.test.js
+++ test/routes/bands.test.js
@@ -9,7 +9,6 @@ describe('bands routes', () => {
         name: 'Susan Metal',
         address: '123 FakeSusan St',
         members: [users[4]._id, users[5]._id, users[6]._id],
-        leaders: [users[0]._id],
         description: 'a band of clarinets'
       })
       .then(res => {
