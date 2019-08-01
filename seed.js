ac676b765397f2d00e1ace95dedf487b943d839d
Zach Butler <zach.standley93@gmail.com>
2019-08-01 15:25:50 -0700

    make more data when we seed outside of testing

--- seed.js
+++ seed.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const seedData = require('./test/seed-data');
 
 mongoose.connection.dropDatabase()
-  .then(() => seedData())
+  .then(() => seedData({ users: 100, concerts: 2, rehearsals: 3, bands: 50 }))
   .then(() => console.log('done'))
   .catch(console.error)
   .finally(() => mongoose.connection.close());
